b4b1bfaaff62ebe9c2e9078f1b741e32e7c961f6
alla.pavlova.96@mail.ru <bitrix@8140055.miet>
2016-12-11 16:05:34 +0300

    Employee sales statistic component has been created

---
+++ local/components/mycompany/stat.employee.detail/lang/en/.description.php
@@ -0,0 +1,5 @@
+<?
+$MESS['NAME_INTRANET'] = "Corporation portal";
+$MESS['NAME_STAT'] = "Sales statistic";
+$MESS['NAME_COMPONENT'] = "Input values sales statistic employees";
+$MESS['DESCRIPTION_COMPONENT'] = "Fill form  sale statistic values employees";
